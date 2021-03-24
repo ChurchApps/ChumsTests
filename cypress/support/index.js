70c9dd59aecf6cfe956d8f359ad70b6163e2fc6b
pranavmalvawala <pranavmalvawala@gmail.com>
2021-03-24 12:59:05 +0530

    fix existing helpers

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,8 +1,3 @@
-import "cypress-wait-until";
 import "./uiHelpers";
 import "./apiHelpers";
 import "./deleteHelpers"
-
-Cypress.Cookies.defaults({
-  preserve: ["ASP.NET_SessionId", ".ASPXAUTH"],
-});
