219d85ec27991da11b2ce5958582fc838b9d1152
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-07-27 16:40:41 +0530

    finish writing household tests

--- cypress/integration/household.spec.ts
+++ cypress/integration/household.spec.ts
@@ -14,8 +14,8 @@ describe("Household", () => {
     throwError()
     addRemove()
     changeRole()
-    // addWithNoChange()
-    // addWithAddressChange()
+    addWithNoChange()
+    addWithAddressChange()
 })
 
 function throwError() {
@@ -73,13 +73,43 @@ function changeRole() {
 }
 
 function addWithNoChange() {
-    it("should add member to household without any change", () => {
+    const people = getPeople(2)
 
+    it("should add member to household without any change", () => {
+        cy.createPeople(people).then((people: PersonInterface[]) => {
+            cy.visit(`/people/${people[0].id}`);
+        })
+        cy.findByText(new RegExp(`${people[0].name.last} household`, "i")).should("exist")
+        cy.findByRole("link", { name: new RegExp(`${people[0].name.first} ${people[0].name.last}`, "i") })
+        cy.findByRole("button", { name: /edithousehold/i }).click()
+        cy.findByRole("button", { name: /addmember/i }).click()
+        cy.findByRole("textbox", { name: /searchbox/i }).type(people[1].name.first || "")
+        cy.findByRole("button", { name: /search/i }).click()
+        cy.findByRole("button", { name: /addperson/i }).click()
+        cy.findByRole("button", { name: /no/i }).click()
+        cy.findByRole("button", { name: /save/i }).click()
+        cy.findByRole("link", { name: new RegExp(`${people[1].name.first} ${people[1].name.last}`) }).click()
+        cy.findByText(new RegExp(`${people[0].contactInfo.city}, ${people[0].contactInfo.state} ${people[0].contactInfo.zip}`)).should("not.exist")
     })
 }
 
 function addWithAddressChange() {
-    it("should add member with address change", () => {
+    const people = getPeople(2)
 
+    it("should add member with address change", () => {
+        cy.createPeople(people).then((people: PersonInterface[]) => {
+            cy.visit(`/people/${people[0].id}`);
+        })
+        cy.findByText(new RegExp(`${people[0].name.last} household`, "i")).should("exist")
+        cy.findByRole("link", { name: new RegExp(`${people[0].name.first} ${people[0].name.last}`, "i") })
+        cy.findByRole("button", { name: /edithousehold/i }).click()
+        cy.findByRole("button", { name: /addmember/i }).click()
+        cy.findByRole("textbox", { name: /searchbox/i }).type(people[1].name.first || "")
+        cy.findByRole("button", { name: /search/i }).click()
+        cy.findByRole("button", { name: /addperson/i }).click()
+        cy.findByRole("button", { name: /yes/i }).click()
+        cy.findByRole("button", { name: /save/i }).click()
+        cy.findByRole("link", { name: new RegExp(`${people[1].name.first} ${people[1].name.last}`) }).click()
+        cy.findByText(new RegExp(`${people[0].contactInfo.city}, ${people[0].contactInfo.state} ${people[0].contactInfo.zip}`)).should("exist")
     })
 }
